ae72a041f3690dccdb3e06c55733b87d8fee1076
Sam Wander <swander1@sva.edu>
2013-10-14 14:03:43 -0400

    Added x and pX logic

--- Motion_Detector/Motion_Detector.ino
+++ Motion_Detector/Motion_Detector.ino
@@ -15,12 +15,25 @@ void setup() {
 void loop() {
     
   int i;
+  int x;
+  int pX;
+  
   for (i = 0; i < 30; i++) {
-  delay(1000);
+    delay(1000);
     xReadings[i] = analogRead(sensorPinX);
     Serial.print(i); 
     Serial.print("\t");  
     Serial.println(xReadings[i]);
+    
+    int x = xReadings[i];
+    int pX = xReadings[i-1];
+    
+    if (x =- pX == 0) {
+      Serial.println("No Motion");
+    } else {
+      Serial.println("Motion");
+   
+    
   }
   
   
