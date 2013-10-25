2f4bc75f3292ff249833f35b3f002b1cbf2ceab0
swndr <Swndr@Sams-MacBook-Pro.local>
2013-10-25 15:35:37 -0400

    Updated with servo array, no use of momentary switch (use reset of Arduino instead)

--- Motion_Detector_V3/Motion_Detector_V3.ino
+++ Motion_Detector_V3/Motion_Detector_V3.ino
@@ -1,26 +1,23 @@
 #include <Servo.h>
 
-// Shift Resistor
+// Shift Register
 
 int latchPin = 5;
 int clockPin = 6;
 int dataPin = 4;
- 
+
 byte leds = 0;
 
 // Servo
 
+int noOfServos = 2;
+int servoPinArray[] = {9,10};
+
 Servo myservo;  // create servo object to control a servo
-int servoPin = 9; // set first servo to PIN 9
 
 // Acceleromator
 
 int sensorPinX = A0; // set X-acceleromator to A0
-
-// Button
-
-const int buttonPin = 2; // pin for button
-boolean buttonState; // default button state
 
 // Time
 
@@ -33,10 +30,7 @@ int hourLength = 30; // set to 3600 for full hour
 
 // Motion
 
-int xReadings[30]; // create an array for 30 secs of readings
-//int xReadings[3600]; // create an array for one hour of readings
-
-int prevReading= 0; // will get one false reading
+int prevReading = 0; // will get one false reading
 int threshold = 3; // tolerance for motion
 
 int motionCounter; // motion count (beyond threshold)
@@ -44,15 +38,18 @@ int motionCounter; // motion count (beyond threshold)
 void setup() {
 
   Serial.begin(9600);
-  myservo.attach(servoPin);  // attaches the servo on PIN 9 to the servo object
-  myservo.write(90);
-  
-  pinMode(buttonPin, INPUT); // attaches button to PIN 2
 
   pinMode(latchPin, OUTPUT); // shift resistor
   pinMode(dataPin, OUTPUT);  // shift resistor
   pinMode(clockPin, OUTPUT); // shift resistor
-  
+
+  for (int i = 0; i < noOfServos; i++) { // this isn't working!
+    myservo.attach(servoPinArray[i]); 
+    myservo.write(90);
+  }
+
+  myservo.attach(servoPinArray[0]);
+
   leds = 0;
   bitSet(leds, h);
   updateShiftRegister();
@@ -63,61 +60,40 @@ void loop() {
 
   unsigned long currentMillis = millis();
   int m; // used for moving Servo
-  buttonState = digitalRead(buttonPin);
-  
+
   if(currentMillis - previousMillis > interval) { 
     previousMillis = currentMillis;
     s++;
     Serial.print("Seconds : ");
     Serial.println(s);
-    
+
     motionCounter += hasMoved();
     Serial.print("Motion count: "); 
     Serial.println(motionCounter);
-    
+
     m = map(motionCounter,0,30,150,30);
     Serial.print("Servo position: "); 
     Serial.println(m);
     Serial.println("\n");
     myservo.write(m);
   }
-  
+
   if(s == hourLength) { 
     s = 0;
     h++;
     Serial.print("Hours : ");
     Serial.println(h);
-    
+
     bitSet(leds, h);
     updateShiftRegister();
-    
+
     motionCounter = 0;
 
-    servoPin++;
-    myservo.attach(servoPin);
-    myservo.write(150);
-    Serial.print("Servo : ");
-    Serial.println(servoPin-8);
+    myservo.attach(servoPinArray[h]);
+//    myservo.write(150);
+    Serial.println("Next Servo");
   } 
- 
-  if (buttonState == 1) {
-     Serial.println("Button pressed");
 
-     for (int i = 9; i <= 11; i++) { // this isn't working!
-       myservo.attach(i); 
-       myservo.write(90);
-       }
-       
-     s = 0;
-     h = 0;
-     leds = 0;
-     updateShiftRegister();
-         
-     servoPin = 9;
-     myservo.attach(servoPin);
-     delay(5000);
-   
-    }
 }
 
 int hasMoved() {
@@ -136,7 +112,8 @@ int hasMoved() {
 
 void updateShiftRegister()
 {
-   digitalWrite(latchPin, LOW);
-   shiftOut(dataPin, clockPin, LSBFIRST, leds);
-   digitalWrite(latchPin, HIGH);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
 }
+
